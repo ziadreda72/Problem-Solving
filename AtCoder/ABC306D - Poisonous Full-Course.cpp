13c354d6e831e55bad7d92913bac4473413a2311
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2023-06-19 23:22:38 +0300

    Update ABC306D - Poisonous Full-Course.cpp

--- AtCoder/ABC306D - Poisonous Full-Course.cpp
+++ AtCoder/ABC306D - Poisonous Full-Course.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 using i64 = long long;
 
 const int N = (int) 3e5 + 10;
@@ -19,19 +17,21 @@ i64 solve(int i , bool cur){
         vis[i][cur] = 1;
         ret = solve(i + 1 , cur);
         if(cur)
-                ret = max(ret , solve(i + 1 , !a[i][0]) + a[i][1]);
+                ret = std::max(ret , solve(i + 1 , !a[i][0]) + a[i][1]);
         else if(!a[i][0])
-                ret = max(ret , solve(i + 1 , 1) + a[i][1]);
+                ret = std::max(ret , solve(i + 1 , 1) + a[i][1]);
         return ret;
 }
 
 int32_t main() {
-        ios::sync_with_stdio(false);cin.tie(nullptr);
-        cout.tie(nullptr);cout.precision(20);
-        cin >> n;
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        std::cout.precision(20);
+        std::cin >> n;
         for (int i = 0; i < n; ++i) {
-                cin >> a[i][0] >> a[i][1];
+                std::cin >> a[i][0] >> a[i][1];
         }
-        cout << solve(0 , 1);
+        std::cout << solve(0 , 1);
         return 0;
 }
