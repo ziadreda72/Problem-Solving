52897226a1d705ab803a51bee4c826a6869eea10
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2023-09-03 17:57:26 +0300

    Update ABC204D - Cooking.cpp

--- AtCoder/ABC204D - Cooking.cpp
+++ AtCoder/ABC204D - Cooking.cpp
@@ -7,28 +7,33 @@ using ld = long double;
 #define F first
 #define S second
 
-const int N = 105 , N2 = 1005;
-
-int n , a[N] , dp[N][N * N2] , S;
-
-int solve(int i , int s){
-        if(i >= n)
-                return max(s , S - s);
-        auto &ret = dp[i][s];
-        if(~ret)
-                return ret;
-        return ret = min(solve(i + 1 , s) , solve(i + 1 , s + a[i]));
-}
-
 int32_t main(){
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
+        int n;
         cin >> n;
+        vector < int > a(n);
+        int S = 0;
         for(int i = 0 ; i < n ; ++i){
                 cin >> a[i];
                 S += a[i];
-        }       
-        memset(dp , -1 , sizeof dp);
-        cout << solve(0 , 0);
+        }
+        vector < vector < bool > > dp(n + 1 , vector < bool > (S + 1 , false));
+        dp[0][0] = 1;
+        for(int i = 0 ; i < n ; ++i){
+                for(int j = 0 ; j <= S ; ++j){
+                        if(dp[i][j]){
+                                dp[i + 1][j] = 1;
+                                dp[i + 1][j + a[i]] = 1;
+                        }
+                }
+        }
+        int ans = S;
+        for(int i = 0 ; i <= S ; ++i){
+                if(dp[n][i]){
+                        ans = min(ans , max(i , S - i));
+                }
+        }
+        cout << ans;
         return 0;
 }
