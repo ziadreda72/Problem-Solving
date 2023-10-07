9ffb7dfbcbbfa6a21f5cd3572fc67b1ef946c57b
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2023-10-07 10:50:11 +0300

    Create Coin Combinations II.cpp

---
+++ CSES/Dynamic Programming/Coin Combinations II.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+
+const int MOD = (int) 1e9 + 7;
+
+int32_t main(){
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n , x;
+        cin >> n >> x;
+
+        vector < int > a(n);
+        for(int i = 0 ; i < n ; ++i) cin >> a[i];
+
+        sort(a.begin() , a.end());
+
+        vector < i64 > dp(x + 1 , 0LL);
+        dp[0] = 1;
+        for(auto c : a){
+                for(int i = 0 ; i <= x ; ++i){
+                        if(i + c <= x){
+                                dp[i + c] += dp[i];
+                                dp[i + c] %= MOD;
+                        }
+                }
+        }
+
+        cout << dp[x];
+
+        return 0;
+}
