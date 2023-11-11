03ce11edd695a352e2ceb90a47c7177c2f862222
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2023-11-11 20:25:21 +0200

    Create ABC328D - Take ABC.cpp

---
+++ AtCoder/ABC328D - Take ABC.cpp
@@ -0,0 +1,58 @@
+A#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+
+int32_t main(){
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        string s;
+        cin >> s;
+
+        int N = (int) s.size();
+
+        string aux;
+        for(int i = 0 ; i < N ;){
+                if(s[i] == 'A'){
+                        if(i + 2 < N && s.substr(i , 3) == "ABC"){
+                                i += 3;
+                        }else{
+                                aux += s[i];
+                                i++;
+                        }
+                }
+
+                if(s[i] == 'B'){
+                        if(aux.empty() || aux.back() != 'A'){
+                                aux += s[i];
+                                cout << aux;
+                                aux.clear();
+                                i++;
+                        }else{
+                                aux += s[i];
+                                i++;
+                        }
+                }
+
+                if(s[i] == 'C'){
+                        if(aux.empty() || aux.back() == 'A'){
+                                aux += s[i];
+                                cout << aux;
+                                aux.clear();
+                                i++;
+                        }else{
+                                string x;
+                                for(int j = 0 ; j < 2 ; ++j){
+                                        x += aux.back();
+                                        aux.pop_back();
+                                }
+                                i++;
+                        }
+                }
+        }
+
+        if(aux.size()) cout << aux;
+
+        return 0;
+}
