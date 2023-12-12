16c92599d162d0d3f094a46980c44eb61997bfc1
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2023-12-12 22:53:56 +0200

    Update 839C. Journey.cpp

--- Codeforces/839C. Journey.cpp
+++ Codeforces/839C. Journey.cpp
@@ -1,64 +1,51 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 using i64 = long long;
+using ld = long double;
 
-#define F first
-#define S second
+const int MAX = (int) 1e5 + 5;
 
 int n;
-vector < vector < int > > g;
-vector < int > vis , d;
-vector < double > prob;
+vector < int > g[MAX];
+int deg[MAX];
 
-void dfs(int u){
-    vis[u] = 1;
-    if(g[u].size() == 1)
-        return;
-    double x = (double) g[u].size() - 1;
-    double p = 1 / x;
-    for(auto v : g[u]){
-        if(!vis[v]){
-            prob[v] = prob[u] * p;
-            d[v] = d[u] + 1;
-            dfs(v);
-        }
-    }
+bool is_leaf(int v){
+        return v != 1 && deg[v] == 1;
 }
 
-int32_t main() {
-    ios::sync_with_stdio(false);cin.tie(nullptr);
-    cout.tie(nullptr);cout.precision(20);
-    cin >> n;
-    if(n == 1){
-        cout << 0;
+ld dfs(int u , int par , ld p , int dep){
+        if(is_leaf(u)){
+                return (ld) 1.0 * p * dep;
+        }
+        ld ret = 0;
+        for(auto v : g[u]){
+                if(v == par) continue;
+                ret += dfs(v , u , p / ((ld) deg[u] - 1) , dep + 1);
+        }
+        return ret;
+}
+
+int32_t main(){
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        cin >> n;
+
+        for(int i = 0 ; i + 1 < n ; ++i){
+                int u , v;
+                cin >> u >> v;
+                g[u].push_back(v);
+                g[v].push_back(u);
+                ++deg[u] , ++deg[v];
+        }
+
+        ld ans = 0.0;
+        for(auto v : g[1]){
+                ans += dfs(v , 1 , (ld) 1.0 / (ld) deg[1] , 1);
+        }
+
+        cout << fixed << setprecision(20) << ans;
+
         return 0;
-    }
-    g = vector < vector < int > > (n);
-    vis.assign(n , 0);
-    d.assign(n , 0);
-    prob.resize(n);
-    for (int i = 0; i < n - 1; ++i) {
-        int u , v;
-        cin >> u >> v;
-        u-- , v--;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-    vis[0] = 1;
-    double x = (double) g[0].size();
-    double p = 1 / x;
-    for(auto i : g[0]){
-        d[i] = 1;
-        prob[i] = p;
-        dfs(i);
-    }
-    double ans = 0;
-    for (int i = 1; i < n; ++i) {
-        if(g[i].size() == 1)
-            ans += d[i] * prob[i];
-    }
-    cout << ans;
-    return 0;
 }
