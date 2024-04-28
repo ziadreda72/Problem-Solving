46ebf3b26c76a3855e68e1349b09a2dafadbfbce
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2024-04-28 08:01:04 +0300

    Update 2256. Minimum Average Difference.cpp

--- LeetCode/2256. Minimum Average Difference.cpp
+++ LeetCode/2256. Minimum Average Difference.cpp
@@ -48,3 +48,63 @@ public:
         return idx;
     }
 };
+
+/*
+    Time: O(n)
+    Space: O(n)
+*/
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        /*
+            Instead of using prefix & suffix sum arrays, we can solve this problem in O(1) space.
+            If we are at index i, we know the sum of the prefix and suffix is the sum of the whole array.
+            So we just need to initialize suffix with sum(nums) then for each i add nums[i] to the prefix
+            and subtract it from the suffix.
+            We must use long long instead of int because at the worst case sum(nums) = 1e10 which won't fit in int.
+        */
+
+        int n = (int) nums.size();
+
+        // Initialize prefix
+        long long prefix = 0LL;
+
+        //Initialize suffix
+        long long suffix = accumulate(nums.begin() , nums.end() , 0LL);
+
+        // We need a variable to hold the minimum difference so far
+        long long mn = INT64_MAX;
+
+        // We also need a variable to hold the index of the minimum so far
+        int idx;
+
+        // Try each index i and try to minimize
+        for (int i = 0 ; i < n ; ++i) {
+            // Add nums[i] to prefix & subtract nums[i] from the suffix
+            prefix += nums[i];
+            suffix -= nums[i];
+
+            // Count how many elements in prefix/suffix
+            int n1 = i + 1; // number of prefix elements
+            int n2 = ((n - i - 1) <= 0) ? 1 : n - i - 1; // number of suffix elements, watch out division by zero
+
+            // Compute Average Difference and try to minimize
+            int diff = abs((prefix / n1) - (suffix / n2));
+            if (diff < mn) {
+                mn = diff;
+                idx = i;
+            }
+        }
+
+        // return idx which holds the index of the minimum difference
+        return idx;
+    }
+};
+
+/*
+    Time: O(n)
+    Space: O(1)
+*/
+
